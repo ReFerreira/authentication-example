407d0cdcef4f3bd02aea88333894f3ba1060cacd
Reinaldo Ferreira <45128389+ReFerreira@users.noreply.github.com>
2021-06-19 18:47:35 -0300

    Auth (#1)
    * authContext
    
    * fake api service
    
    * form implementation
    
    * dashboard getting context
    
    * config

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,19 @@
-import { Fragment } from 'react'
-import Head from 'next/head'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Fragment, useContext } from "react";
+import Head from "next/head";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { AuthContext } from "../context/AuthContext";
 
-const navigation = ['Dashboard', 'Team', 'Projects', 'Calendar', 'Reports']
-const profile = ['Your Profile', 'Settings']
+const navigation = ["Dashboard", "Team", "Projects", "Calendar", "Reports"];
+const profile = ["Your Profile", "Settings"];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div>
       <Head>
@@ -36,7 +39,10 @@ export default function Dashboard() {
                         itemIdx === 0 ? (
                           <Fragment key={item}>
                             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                            <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                            <a
+                              href="#"
+                              className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
                               {item}
                             </a>
                           </Fragment>
@@ -69,7 +75,7 @@ export default function Dashboard() {
                               <span className="sr-only">Open user menu</span>
                               <img
                                 className="h-8 w-8 rounded-full"
-                                src="https://github.com/diego3g.png"
+                                src={user?.avatar_url}
                                 alt=""
                               />
                             </Menu.Button>
@@ -94,8 +100,8 @@ export default function Dashboard() {
                                     <a
                                       href="#"
                                       className={classNames(
-                                        active ? 'bg-gray-100' : '',
-                                        'block px-4 py-2 text-sm text-gray-700'
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-sm text-gray-700"
                                       )}
                                     >
                                       {item}
@@ -106,7 +112,7 @@ export default function Dashboard() {
                               <Menu.Item>
                                 <a
                                   href="#"
-                                  className='block px-4 py-2 text-sm text-gray-700'
+                                  className="block px-4 py-2 text-sm text-gray-700"
                                 >
                                   Sign out
                                 </a>
@@ -138,7 +144,10 @@ export default function Dashboard() {
                   itemIdx === 0 ? (
                     <Fragment key={item}>
                       {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                      <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
+                      <a
+                        href="#"
+                        className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                      >
                         {item}
                       </a>
                     </Fragment>
@@ -158,13 +167,17 @@ export default function Dashboard() {
                   <div className="flex-shrink-0">
                     <img
                       className="h-10 w-10 rounded-full"
-                      src="https://github.com/diego3g.png"
+                      src={user?.avatar_url}
                       alt=""
                     />
                   </div>
                   <div className="ml-3">
-                    <div className="text-base font-medium leading-none text-white">Diego Fernandes</div>
-                    <div className="text-sm font-medium leading-none text-gray-400">diego@rocketseat.com.br</div>
+                    <div className="text-base font-medium leading-none text-white">
+                      Diego Fernandes
+                    </div>
+                    <div className="text-sm font-medium leading-none text-gray-400">
+                      diego@rocketseat.com.br
+                    </div>
                   </div>
                   <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                     <span className="sr-only">View notifications</span>
@@ -209,5 +222,5 @@ export default function Dashboard() {
         </div>
       </main>
     </div>
-  )
+  );
 }
